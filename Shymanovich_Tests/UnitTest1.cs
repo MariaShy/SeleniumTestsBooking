2cca23424385fae8b6764cc6f3d009f49ad2db48
Maria Shy <24011989@mail.ru>
2021-04-16 09:50:15 +0300

    SignIn test

---
+++ Shymanovich_Tests/UnitTest1.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace Shymanovich_Tests
+{
+    /* 
+     * for Booking.com:
+     * Change currency / language
+     * Check the moving to tiskets purchase page
+     * Check the availability of personal page
+     * Check the filter (choose a city, trip in a week, arrival +2 days, 2 adults & 1 child, 1 room)
+     */
+    public class Tests
+    {        
+        private IWebDriver driver;
+        #region 1st test
+        //private readonly By _        
+        #endregion
+
+        #region 3d test
+        private readonly By _signInButton = By.XPath("//*[@id='b2indexPage']/header/nav[1]/div[2]/div[6]/a/span");
+        private readonly By _loginInput = By.XPath("//*[@id='username']");
+        private readonly By _continueButton = By.XPath("//*[@id='root']/div/div[2]/div[1]/div/div/div/div/div/div/form/div[3]/button/span");
+        private readonly By _passwordInput = By.XPath("//*[@id='password']");
+        private readonly By _enterButton = By.XPath("//*[@id='root']/div/div[2]/div[1]/div/div/div/div/div/div/form/button/span");
+        private readonly By _actualSignIn = By.XPath("//*[@id='profile-menu-trigger--title']");
+
+        private const string userLogin = "24011989m@mail.ru";
+        private const string userPassword = "/m*gq6ie55A,!H&";
+        private const string expectedSignIn = "Maryia Shatsila";
+        #endregion
+
+        [SetUp]
+        public void Setup()
+        {            
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.booking.com"); 
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void Test1() //Change currency / language
+        {
+            
+        }
+
+        [Test]
+        public void Test2() //Check the moving to tiskets purchase page
+        {
+            
+        }
+
+        [Test]
+        public void Test3() //Check the availability of personal page
+        {
+            var signIn = driver.FindElement(_signInButton);
+            signIn.Click();
+            Thread.Sleep(500);
+
+            var loginInput = driver.FindElement(_loginInput);
+            loginInput.SendKeys(userLogin);
+
+            var continueButton = driver.FindElement(_continueButton);
+            continueButton.Click();
+            Thread.Sleep(500);
+
+            var passwordInput = driver.FindElement(_passwordInput);
+            passwordInput.SendKeys(userPassword);
+
+            var enterButton = driver.FindElement(_enterButton);
+            enterButton.Click();
+            Thread.Sleep(500);
+
+            var actualSignIn = driver.FindElement(_actualSignIn);
+
+            Assert.AreEqual(expectedSignIn, actualSignIn.Text, "Could not sign in!");
+        }
+
+        [Test]
+        public void Test4() //Check the filter (choose a city, trip in a week, arrival +2 days, 2 adults & 1 child, 1 room)
+        {
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
